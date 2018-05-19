a8d7e1512a4a48a3dc37b5a7fb2479550eccda91
srujan964 <34967423+srujan964@users.noreply.github.com>
2018-05-19 23:33:19 +0530

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,35 @@
+//Variable declarations
+let width = $("#inputWeight");
+let height = $("#inputHeight");
+let grid = $("#pixelCanvas");
+let color = $("#colorPicker");
+
+//Submit button event listener.
+//Clears the grid first, and then creates a new on based on inputs: width and height.
+$('form').on('submit', function(event) {
+    event.preventDefault();
+    resetGrid();
+    makeGrid();
+});
+
+//Function to clear the grid.
+function resetGrid() {
+    grid.empty();
+}
+
+
+//Function to create a new grid, and adds an event listener to color these cells.
+function makeGrid() {
+    for(let rowIndex = 0; rowIndex < height.val(); rowIndex++) {
+        var row = "<tr>";
+        for(let colIndex = 0; colIndex < width.val(); colIndex++) {
+            row += "\n<td></td>";
+        }
+        row += "\b</tr>";
+        grid.append(row);
+        $("td").on("click", function setCell() {
+            $(this).attr("style", "background-color: " + color.val());
+        });
+    }
+}
+
